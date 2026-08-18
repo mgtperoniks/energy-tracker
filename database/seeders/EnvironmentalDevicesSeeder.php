05ac9b17e403cb4e5ff9ef31f6630c23036dc94c
ppic2 <ppic2@pkshp.com>
2026-08-18 19:03:32 +0700

    feat: add SHT40 Lab 3 and Lab 4 environmental sensors

--- database/seeders/EnvironmentalDevicesSeeder.php
+++ database/seeders/EnvironmentalDevicesSeeder.php
@@ -87,5 +87,57 @@ class EnvironmentalDevicesSeeder extends Seeder
             $d29->api_token = Str::random(60);
             $d29->save();
         }
+
+        // 4. Provision Device 30 (SHT40 Lab 3)
+        $d30 = Device::find(30);
+        if ($d30) {
+            // Update existing record
+            $d30->name = 'SHT40 Lab 3';
+            $d30->type = 'temperature_sensor';
+            $d30->slave_id = 54;
+            $d30->communication_type = 'TCP';
+            $d30->status = 1;
+            if (empty($d30->api_token)) {
+                $d30->api_token = Str::random(60);
+            }
+            $d30->save();
+        } else {
+            // Create new record with ID 30
+            $d30 = new Device();
+            $d30->id = 30;
+            $d30->name = 'SHT40 Lab 3';
+            $d30->type = 'temperature_sensor';
+            $d30->slave_id = 54;
+            $d30->communication_type = 'TCP';
+            $d30->status = 1;
+            $d30->api_token = Str::random(60);
+            $d30->save();
+        }
+
+        // 5. Provision Device 31 (SHT40 Lab 4)
+        $d31 = Device::find(31);
+        if ($d31) {
+            // Update existing record
+            $d31->name = 'SHT40 Lab 4';
+            $d31->type = 'temperature_sensor';
+            $d31->slave_id = 55;
+            $d31->communication_type = 'TCP';
+            $d31->status = 1;
+            if (empty($d31->api_token)) {
+                $d31->api_token = Str::random(60);
+            }
+            $d31->save();
+        } else {
+            // Create new record with ID 31
+            $d31 = new Device();
+            $d31->id = 31;
+            $d31->name = 'SHT40 Lab 4';
+            $d31->type = 'temperature_sensor';
+            $d31->slave_id = 55;
+            $d31->communication_type = 'TCP';
+            $d31->status = 1;
+            $d31->api_token = Str::random(60);
+            $d31->save();
+        }
     }
 }
